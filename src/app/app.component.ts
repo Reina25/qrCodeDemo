3db50035aa236344edaf0b3b7ce2b05b0ebe4aff
Reina25 <reinasalam5@gmail.com>
2023-11-30 14:07:11 +0200

    qr code generation
    goes to iConnect login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qrCodeDemo';
+
+  myAngularxQrCode: string ;
+
+  constructor () {
+    this.myAngularxQrCode = 'https://icas.bau.edu.lb:8443/cas/login?service=https://mis.bau.edu.lb/web/v12/iconnectv12/cas/sso.aspx';
+  }
+
+  div1:boolean=true;
+  div2:boolean=false;
+  div3:boolean=false;
+  div4:boolean=false;
+
+  ShowDiv1Function(){
+    this.div1=true;
+    this.div2=false;
+    this.div3=false;
+    this.div4=false;
+  }
+
+  ShowDiv2Function(){
+    this.div1=false;
+    this.div2=true;
+    this.div3=false;
+    this.div4=false;
+  }
+
+  ShowDiv3Function(){
+    this.div1=false;
+    this.div2=false;
+    this.div3=true;
+    this.div4=false;
+  }
+
+  ShowDiv4Function(){
+    this.div1=false;
+    this.div2=false;
+    this.div3=false;
+    this.div4=true;
+  }
+
+  ShowDiv5Function(){
+    
+  }
 }
