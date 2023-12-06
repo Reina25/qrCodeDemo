eddf30f1e16e6c778651a59db41d98fda049e88e
Reina25 <reinasalam5@gmail.com>
2023-12-06 09:40:30 +0200

    code cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-// import { QRCodeOverlay } from "@progress/kendo-angular-barcodes";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,18 +9,13 @@ export class AppComponent {
 
   qrCode1: string ;
   qrCode2: string;
-
-  // public overlay: QRCodeOverlay = {
-  //   type: "image",
-  //   imageUrl:
-  //     "https://demos.telerik.com/kendo-ui/content/shared/images/site/kendoka-cta.svg",
-  //   width: 60,
-  //   height: 60,
-  // };
+  qrCode3: string;
 
   constructor () {
     this.qrCode1 = 'https://icas.bau.edu.lb:8443/cas/login?service=https://mis.bau.edu.lb/web/v12/iconnectv12/cas/sso.aspx';
     this.qrCode2 = 'https://emn178.github.io/online-tools/md5.html';
+    this.qrCode3 = 'https://emn178.github.io/online-tools/md5.html';
+
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { QRCodeModule } from 'angularx-qrcode';
-
-// import { BarcodesModule } from "@progress/kendo-angular-barcodes";
-// import { ButtonsModule } from "@progress/kendo-angular-buttons";
 
 @NgModule({
   declarations: [
@@ -17,8 +13,7 @@ import { QRCodeModule } from 'angularx-qrcode';
     BrowserModule,
     AppRoutingModule,
     QRCodeModule,
-    // BarcodesModule, 
-    // ButtonsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
